9cecb54222514b3ebcd99b97f462a341afcfeec9
krinalbodra-oss <krinalbodra@gmail.com>
2026-04-30 15:09:59 +0530

    Create practical.java

---
+++ practical.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class BubbleSortTime {
+    public static void main(String[] args) {
+        int size = 10;
+        int[] arr = new int[size];
+        Random r = new Random();
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = r.nextInt(100);
+        }
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        long start = System.nanoTime();
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            System.out.println("Pass " + (i + 1) + ":");
+            printArray(arr);
+        }
+
+        long end = System.nanoTime();
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+
+        System.out.println("Execution Time: " + (end - start) + " ns");
+    }
+
+    static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
