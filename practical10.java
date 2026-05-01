a98c6a0292d62d207a3a2ffde0e907a11ba39194
krinalbodra-oss <krinalbodra@gmail.com>
2026-05-01 19:52:29 +0530

    Create practical10.java

---
+++ practical10.java
@@ -0,0 +1,27 @@
+public class KnapsackDP {
+    public static void main(String[] args) {
+        int[] weights = {2, 3, 4, 5};
+        int[] values = {3, 4, 5, 6};
+        int capacity = 5;
+
+        int n = values.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(
+                        values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                        dp[i - 1][w]
+                    );
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        System.out.println("Maximum value: " + dp[n][capacity]);
+    }
+}
